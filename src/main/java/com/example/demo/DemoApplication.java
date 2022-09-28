17f76473deb0411ef9a39e533ad128f172780b8a
Barry Lagerweij <b.lagerweij@carepay.com>
2022-09-29 00:21:57 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Main method");
+	}
+
+        public static String hello() {
+            return "hello, world";
+        }
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+class DemoApplicationTests {
+
+	void contextLoads() {
+            System.out.println(DemoApplication.hello());
+	}
+
+}
